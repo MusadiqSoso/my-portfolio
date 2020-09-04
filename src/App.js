0250e1bd54f5f5d5b122d9c16066bce214542477
w3bh4ck <amadiluckysampson@gmail.com>
2020-09-04 16:27:58 +0100

    add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import Home from '../src/pages/home /home';
-import About from '../src/pages/about/about';
-import Contact from '../src/pages/contact/contact';
-import Resume from '../src/pages/resume/resume'
-;function App() {
-  return (
-    <div className="App">
-      <Home/>
-      
-    </div>
-  );
+import React from "react";
+import Home from "../src/pages/home /home";
+import About from "../src/pages/about/about";
+import Contact from "../src/pages/contact/contact";
+import Resume from "../src/pages/resume/resume";
+function App() {
+	return (
+		<div className="App">
+			<Home />
+		</div>
+	);
 }
 
 export default App;
