76b5905dbfc4db10344df75a8985915649a5c115
MusadiqSoso <musadiq.soso@uoit.net>
2020-09-08 15:14:16 -0400

    Updates

---
+++ precache-manifest.32ff633e9dae1096391f69fc3a9f949f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0efdfdb5f6c97416cb06d9cdf6abdf7",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "9f954d03136525f17caf",
+    "url": "/my-portfolio/static/css/main.69d99e9f.chunk.css"
+  },
+  {
+    "revision": "bd04b151e76a3cb53f90",
+    "url": "/my-portfolio/static/js/2.425a7230.chunk.js"
+  },
+  {
+    "revision": "a6303f07744f642fd12b9b4c3c7cb8f2",
+    "url": "/my-portfolio/static/js/2.425a7230.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f954d03136525f17caf",
+    "url": "/my-portfolio/static/js/main.db4fa42b.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "160b1515d380bec1684b75e8f52e7bb7",
+    "url": "/my-portfolio/static/media/MusadiqFinal.160b1515.pdf"
+  },
+  {
+    "revision": "27ab047eef87c02bd205865f05e19156",
+    "url": "/my-portfolio/static/media/bals.27ab047e.jpg"
+  },
+  {
+    "revision": "f0e39a3367ac4d97a48a192393c19e52",
+    "url": "/my-portfolio/static/media/fbg2.f0e39a33.jpg"
+  },
+  {
+    "revision": "3ec3891b37f8b0765b554f10806f0b3f",
+    "url": "/my-portfolio/static/media/male1.3ec3891b.png"
+  }
+]);
